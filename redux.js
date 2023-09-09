5a642c4769b924ba385ad09d4655077855b97617
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-09-09 16:47:29 +0530

    add

---
+++ redux.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+const CountReducer=(state={counter:0},action)=>{
+if(action.type==='increment'){
+    return{
+    counter:state.counter+1,
+    }
+}
+if(action.type==='decrement'){
+    return{
+   counter:state.counter-1,
+    }
+};
+return state;
+
+}
+const store=createStore(CountReducer);
+export default store;
